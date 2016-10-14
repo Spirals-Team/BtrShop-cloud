6fb480a09b47f43e76340857394b8d54b70cf5ef
Oupsla <nicolas_delperdange@hotmail.com>
2016-10-14 10:40:49 +0200

    chore(test): adding dockunit, sample-test and travis
    chore(test): adding dockunit, sample-test and travis
    
    chore(test): adding dockunit, sample-test and travis
    
    chore(test): adding dockunit, sample-test and travis
    
    chore(test): adding dockunit, sample-test and travis
    
    chore(test): adding dockunit, sample-test and travis
    
    chore(test): adding dockunit, sample-test and travis

---
+++ test/sample-test.js
@@ -0,0 +1,29 @@
+var supertest = require("supertest");
+var should = require("should");
+
+// This agent refers to PORT where program is runninng.
+
+var server = supertest.agent("http://localhost:8080");
+
+
+
+
+describe("SAMPLE unit test",function(){
+
+  // #1 should return home page
+
+  it("should return home page",function(done){
+
+    // calling home page api
+    server
+    .get("/")
+    .expect("Content-type",/json/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      res.status.should.equal(200);
+      done();
+    });
+
+  });
+
+});
