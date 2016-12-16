cdf4ce5c54af5044702a20225f7537dac075c6ad
Nicolas D <nicolas_delperdange@hotmail.com>
2016-12-16 14:37:52 +0100

    fix(beacon): changing url for beacon

--- model/beacon/beacon-facade.js
+++ model/beacon/beacon-facade.js
@@ -4,15 +4,12 @@ const beaconSchema  = require('./beacon-schema');
 class BeaconModel extends Model {
 
   updateBeacons(beaconsList) {
-
-    console.log('facade');
-
     return beaconSchema
       .remove({})
       .then(() => beaconSchema
           .create(beaconsList));
-
   }
+  
 }
 
 module.exports = new BeaconModel(beaconSchema);
--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const product  = require('./model/product/product-router');
 const beacon   = require('./model/beacon/beacon-router');
 
 router.use('/products', product);
-router.use('/beacon', beacon);
+router.use('/beacons', beacon);
 
 
 module.exports = router;
